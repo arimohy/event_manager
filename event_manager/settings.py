38fbc76dbc90b24c81b7ae7502608708535c756f
arimohy <arimohy@gmail.com>
2024-12-12 16:52:43 -0500

    Se actualizo requeriments y se agrego filtros

--- event_manager/settings.py
+++ event_manager/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     'rest_framework',
     'drf_yasg',
+    'django_filters',
     "events",
 ]
 
@@ -131,3 +132,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+}
--- events/views.py
+++ events/views.py
@@ -1,6 +1,8 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import generics, filters
 from .models import Event, Registration
 from .serializers import EventSerializer, RegistrationSerializer
+from .filters import EventFilter
 
 class EventList(generics.ListCreateAPIView):
     queryset = Event.objects.all()
@@ -16,3 +18,8 @@ class RegisterEvent(generics.CreateAPIView):
     queryset = Registration.objects.all()
     serializer_class = RegistrationSerializer
 
+class FilteredEventsView(generics.ListAPIView):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = EventFilter
